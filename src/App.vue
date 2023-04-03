d6503cb251580219234ffd8115857bb7c4a6416f
Luissa Valencia <luissam1625@outlook.es>
2023-04-02 20:46:02 -0500

    Entrega

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <img src="./assets/marvel-background.jpg">
+    </div>
+    <List/>
+  </div>
+</template>
+
+<script>
+import List from './components/List'
+
+export default {
+  name: 'App',
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #af9fd6;
+}
+#nav{
+  background-color: #e92227;
+  height: 160px;
+}
+img{
+  width: 20%;
+  border: 5px solid black;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,61 @@
+<template>
+<section>
+    <div class="container-row">
+      <div id="liste" class="container-wrap" >
+        <Character v-for="character in characters" :key="character.id" :character="character"/>
+      </div>
+    </div>
+   
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+import Character from '../components/Character'
+
+export default {
+  name: 'List',
+
+  components: {
+    Character
+  },
+
+  data() {
+    return {
+      characters:[]
+    }
+  },
+
+  methods:{
+    
+      },
+  beforeCreate() {
+    axios.get(`https://gateway.marvel.com:443/v1/public/characters?apikey=2678c2caaeb56f87dd8ef031853b93ca&hash=6d5eafa43d3aab85e8c20291b547e01d`)
+      .then((response) => {this.characters = response.data.data.results;
+              console.log(response.data.data.results)})
+      // .catch( (error) => console.error(error));
+  }
+}
+</script>
+
+<style>
+.container-row {
+  display : flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.container-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+#liste{
+  width: 1030px;
+  height: 1600px;
+  margin-top: 50px;
+}
+
+</style>
